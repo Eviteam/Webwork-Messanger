345995a0535af09d3a855a7bb24e31e654b90288
Gev <safaryangevor1995@gmail.com>
2021-04-08 18:35:08 +0400

    changed structure with loader

--- frontend/src/app/services/app/app.service.ts
+++ frontend/src/app/services/app/app.service.ts
@@ -14,16 +14,27 @@ export class AppService {
     private storageService: LocalStorageService
   ) { }
 
-  public setCurrenUser(): void {
-    this.activatedRoute.queryParams
+  public setCurrenUser() {
+    return new Promise((resolve, reject) => {
+      this.activatedRoute.queryParams
       .subscribe(param => {
         if (param.user_id) {
           this.apiService.post(`/api/current_user/${param.user_id}`)
-            .subscribe(data => {
-              this.storageService.setItem('team_id', data.team.team_id);
-              this.storageService.setItem('user_id', data.user_id)
-            })
+            .subscribe(
+              data => {
+                this.storageService.setItem('team_id', param.team_id);
+                this.storageService.setItem('user_id', param.user_id)
+                this.storageService.setItem('selectedUser', param.user_id)
+                const team_id = this.storageService.getItem('team_id')
+                const user_id = this.storageService.getItem('user_id')
+                const selectedUser = this.storageService.getItem('selectedUser')
+                const userData = {team_id, user_id, selectedUser}
+                resolve(userData)
+              },
+              err => reject(err)
+            )
         }
       })
+    })
   }
 }
---
+++ frontend/src/app/views/loader/loader.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from 'src/app/services/app/app.service';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent implements OnInit {
+
+  constructor(
+    private appService: AppService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.appService.setCurrenUser()
+      .then((data: any) =>  {
+        console.log(data, "data")
+        this.router.navigateByUrl(`/main/${data.selectedUser}`)
+      })
+      .catch(err => console.log(err, "err"))
+  }
+
+}
