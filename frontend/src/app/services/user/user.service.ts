a2f6cc118cd9b4048bca8898b5f96f5adb55f871
tigran <tikomanukyan44@gmail.com>
2021-06-24 11:14:51 +0400

    user-status

--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -15,11 +15,11 @@ export class UserService {
   constructor(private apiService: ApiService) { }
 
   public getAllUsers(user_id: string): Observable<Team> {
-    return this.apiService.get(`/api/users/${user_id}`)
+    return this.apiService.get(`/api/users/${user_id}`);
   }
 
   public getSingleUser(user_id: string): Observable<User> {
-    return this.apiService.get(`/api/users/single_user/${user_id}`)
+    return this.apiService.get(`/api/users/single_user/${user_id}`);
   }
 
   public setMessageIsRead(message: boolean): void {
